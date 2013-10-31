690c97d6dbf9e366a03d57eb66cdd99a7b640e23
Zmo <simo.kolppo@gmail.com>
2013-10-31 23:03:34 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treeniapp::Application.config.secret_key_base = '7199d5e61143fb6bf11094df9d7974bdc2aa2df34f1205e78e281518288bd7c53a332f2d25929ad2f64ad5bd32fb4e9e3cd262c271c7111d1000bf6074f5959f'
